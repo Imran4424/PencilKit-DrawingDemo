0c6f01cebe5b45ff86dd0ec40d55c077ce7fdd1d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-12-24 22:07:47 +0600

    project init

---
+++ DrawingDemo/DrawingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingDemoApp.swift
+//  DrawingDemo
+//
+//  Created by Shah Md Imran Hossain on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
